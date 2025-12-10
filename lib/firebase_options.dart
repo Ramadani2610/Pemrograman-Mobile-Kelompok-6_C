495341a3808c7f951ad6cb6e6d418a514df5c28c
Ramadani2610 <ramadanimakassar180219@gmail.com>
2025-12-11 03:22:39 +0800

    be

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -1,64 +1,73 @@
+// lib/firebase_options.dart
 import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
+import 'package:flutter/foundation.dart'
+    show defaultTargetPlatform, kIsWeb, TargetPlatform;
 
+/// Default [FirebaseOptions] for use with your Firebase apps.
+///
+/// Example:
+/// ```dart
+/// await Firebase.initializeApp(
+///   options: DefaultFirebaseOptions.currentPlatform,
+/// );
+/// ```
 class DefaultFirebaseOptions {
+  static FirebaseOptions get currentPlatform {
+    if (kIsWeb) {
+      return web;
+    }
+    switch (defaultTargetPlatform) {
+      case TargetPlatform.android:
+        return android;
+      case TargetPlatform.iOS:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions have not been configured for ios - '
+          'you can reconfigure this by running the FlutterFire CLI again.',
+        );
+      case TargetPlatform.macOS:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions have not been configured for macos - '
+          'you can reconfigure this by running the FlutterFire CLI again.',
+        );
+      case TargetPlatform.windows:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions have not been configured for windows - '
+          'you can reconfigure this by running the FlutterFire CLI again.',
+        );
+      case TargetPlatform.linux:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions have not been configured for linux - '
+          'you can reconfigure this by running the FlutterFire CLI again.',
+        );
+      default:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions are not supported for this platform.',
+        );
+    }
+  }
+
+  // =================================================================
+  // BAGIAN WEB (Isi dengan data dari Layar "Config" di Website Firebase)
+  // =================================================================
   static const FirebaseOptions web = FirebaseOptions(
-    apiKey: 'AIzaSyDummyKeyForDevelopment',
-    appId: '1:000000000000:web:0000000000000000000000',
-    messagingSenderId: '000000000000',
-    projectId: 'spareapp-unhas-dev',
-    authDomain: 'spareapp-unhas-dev.firebaseapp.com',
-    databaseURL: 'https://spareapp-unhas-dev.firebaseio.com',
-    storageBucket: 'spareapp-unhas-dev.appspot.com',
-    measurementId: 'G-XXXXXXXXXX',
+    apiKey: 'AIzaSyDcdKIDCCblHuPlqFaAAY5Eyhfee12fLzk',
+    appId: '1:694348068686:web:42038c3f03fe5de83c901f',
+    messagingSenderId: '694348068686',
+    projectId: 'spare-app-unhas', // Sesuaikan jika di web beda
+    authDomain: 'spare-app-unhas.firebaseapp.com', // Sesuaikan
+    storageBucket: 'spare-app-unhas.firebasestorage.app', // Sesuaikan
+    // measurementId tidak wajib, boleh dihapus kalau error
   );
 
+  // =================================================================
+  // BAGIAN ANDROID (Biarkan Dummy dulu tidak apa-apa untuk Chrome)
+  // Karena di Android kita sudah pakai google-services.json
+  // =================================================================
   static const FirebaseOptions android = FirebaseOptions(
-    apiKey: 'AIzaSyDummyKeyForDevelopment',
-    appId: '1:000000000000:android:0000000000000000000000',
-    messagingSenderId: '000000000000',
-    projectId: 'spareapp-unhas-dev',
-    databaseURL: 'https://spareapp-unhas-dev.firebaseio.com',
-    storageBucket: 'spareapp-unhas-dev.appspot.com',
-  );
-
-  static const FirebaseOptions ios = FirebaseOptions(
-    apiKey: 'AIzaSyDummyKeyForDevelopment',
-    appId: '1:000000000000:ios:0000000000000000000000',
-    messagingSenderId: '000000000000',
-    projectId: 'spareapp-unhas-dev',
-    databaseURL: 'https://spareapp-unhas-dev.firebaseio.com',
-    storageBucket: 'spareapp-unhas-dev.appspot.com',
-    iosClientId:
-        '000000000000-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx.apps.googleusercontent.com',
-    iosBundleId: 'com.example.spareappUnhas',
-  );
-
-  static const FirebaseOptions macos = FirebaseOptions(
-    apiKey: 'AIzaSyDummyKeyForDevelopment',
-    appId: '1:000000000000:macos:0000000000000000000000',
-    messagingSenderId: '000000000000',
-    projectId: 'spareapp-unhas-dev',
-    databaseURL: 'https://spareapp-unhas-dev.firebaseio.com',
-    storageBucket: 'spareapp-unhas-dev.appspot.com',
-    iosClientId:
-        '000000000000-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx.apps.googleusercontent.com',
-    iosBundleId: 'com.example.spareappUnhas',
-  );
-
-  static const FirebaseOptions windows = FirebaseOptions(
-    apiKey: 'AIzaSyDummyKeyForDevelopment',
-    appId: '1:000000000000:windows:0000000000000000000000',
-    messagingSenderId: '000000000000',
-    projectId: 'spareapp-unhas-dev',
-    databaseURL: 'https://spareapp-unhas-dev.firebaseio.com',
-    storageBucket: 'spareapp-unhas-dev.appspot.com',
-  );
-
-  static const FirebaseOptions linux = FirebaseOptions(
-    apiKey: 'AIzaSyDummyKeyForDevelopment',
-    appId: '1:000000000000:linux:0000000000000000000000',
-    messagingSenderId: '000000000000',
-    projectId: 'spareapp-unhas-dev',
+    apiKey: 'AIzaSyDcdKIDCCblHuPlqFaAAY5Eyhfee12fLzk',
+    appId: '1:694348068686:android:bde844c53623e0353c901f',
+    messagingSenderId: '694348068686',
+    projectId: 'spare-app-unhas',
     databaseURL: 'https://spareapp-unhas-dev.firebaseio.com',
     storageBucket: 'spareapp-unhas-dev.appspot.com',
   );
